01e06d880e415077d831b06bf0e841da202b51a2
Naveen Chakaravarthy <naveen.chakaravarthy@freshworks.com>
2018-08-12 17:26:15 +0530

    CrossOrigin Updates

--- src/main/java/com/springboot/besant/course/CourseController.java
+++ src/main/java/com/springboot/besant/course/CourseController.java
@@ -3,12 +3,14 @@ package com.springboot.besant.course;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4300", maxAge=3600)
 @RestController
 public class CourseController {
 	
